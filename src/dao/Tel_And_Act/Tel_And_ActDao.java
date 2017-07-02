ac706cbac9b48c197c1572137d583f03922e2526
xiaoming <965537330@qq.com>
2017-07-02 22:10:04 +0800

    1

--- src/dao/Tel_And_Act/Tel_And_ActDao.java
+++ src/dao/Tel_And_Act/Tel_And_ActDao.java
@@ -38,14 +38,14 @@ public class Tel_And_ActDao extends CommonDAO implements IBaseDao{
 	}
 	
 	public List<Map<String, Object>> getAllActiveInfo() throws Exception{
-		String sql = "select * from v_active" + "where state = '已同意'";
+		String sql = "select * from v_active" + " where state = '已同意'";
 		System.out.println("----------------------------------------");
 		System.out.println("SQL:" + sql);
 		return excuteQuery(sql, null);
 	} 
 	
 	public List<Map<String, Object>> getAllActiveInfoByIndex(int index) throws Exception{
-		String sql = "select * from v_active where state = '已提交' limit " + (index - 1) * 10 + ",10";
+		String sql = "select * from v_active where state = '已同意' limit " + (index - 1) * 10 + ",10";
 		System.out.println("----------------------------------------");
 		System.out.println("SQL:" + sql);
 		return excuteQuery(sql, null);
