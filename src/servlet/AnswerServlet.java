c2ea2fd2224e5b29fa585727e4666a09aefdaec4
xiaoming <965537330@qq.com>
2017-07-04 21:22:07 +0800

    问题答案

---
+++ src/servlet/AnswerServlet.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import service.Answer.AnswerService;
+import service.Question.QuestionService;
+import util.sendToHtml.SendToHtml;
+
+/**
+ * Servlet implementation class AnswerServlet
+ */
+@WebServlet("/AnswerServlet")
+public class AnswerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnswerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("textml;charset=utf-8");		
+		response.setContentType("text/html");
+		response.setHeader("Content-type", "text/html;charset=UTF-8");
+		
+		if("display".equals(request.getParameter("info"))){
+			int page = Integer.parseInt(request.getParameter("page")+"");
+			Long userId = Long.parseLong(request.getParameter("userId") + "");
+			AnswerService answerService = new AnswerService();
+			
+			List<Map<String, Object>> list = answerService.getInfoByPage(page);
+			
+			if(list != null){
+				JSONArray json = JSONArray.fromObject(list);
+				SendToHtml.send(json, response);
+			}
+		}
+	}
+
+}
---
+++ src/servlet/QuestionServlet.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import service.Question.QuestionService;
+import service.Tel_And_Act.Tel_And_ActService;
+import util.sendToHtml.SendToHtml;
+
+/**
+ * Servlet implementation class QuestionAnswer
+ */
+@WebServlet("/QuestionAnswer")
+public class QuestionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuestionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("textml;charset=utf-8");		
+		response.setContentType("text/html");
+		response.setHeader("Content-type", "text/html;charset=UTF-8");
+		
+		if("display".equals(request.getParameter("info"))){
+			int page = Integer.parseInt(request.getParameter("page")+"");
+			Long userId = Long.parseLong(request.getParameter("userId") + "");
+			QuestionService questionService = new QuestionService();
+			
+			List<Map<String, Object>> list = questionService.getInfoByPage(page);
+			
+			if(list != null){
+				JSONArray json = JSONArray.fromObject(list);
+				SendToHtml.send(json, response);
+			}
+		}
+	}
+
+}
